a01f86f71540ef298331b64ef5883ff7d775c72f
warugaki <warugaki.web.developer@gmail.com>
2022-02-28 23:21:50 +0900

    グローバルスタイルを誤って削除したので追加

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { Global } from '@emotion/react';
+import globalStyle from './styles/global';
 
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Global styles={globalStyle} />
     <BrowserRouter>
       <App />
     </BrowserRouter>
