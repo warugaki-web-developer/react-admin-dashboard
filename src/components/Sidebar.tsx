e7cbd9de5b0b4083e3dd4536454f848a521d6edb
warugaki <warugaki.web.developer@gmail.com>
2022-03-01 00:27:12 +0900

    logoとプロジェクト名を追加

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,17 +1,25 @@
 import React from 'react';
 import SidebarLink from './SidebarLink';
 
+import logo from '../assets/logo.svg';
+
 const Sidebar = () => (
-  <nav>
-    <ul>
-      <li>
-        <SidebarLink to="/">Dashboard</SidebarLink>
-      </li>
-      <li>
-        <SidebarLink to="/customers">Customers</SidebarLink>
-      </li>
-    </ul>
-  </nav>
+  <div className="sidebar">
+    <div className="sidebar__logo">
+      <img src={logo} alt="logo" />
+      <h1>React Admin Dashboard</h1>
+    </div>
+    <nav>
+      <ul>
+        <li>
+          <SidebarLink to="/">Dashboard</SidebarLink>
+        </li>
+        <li>
+          <SidebarLink to="/customers">Customers</SidebarLink>
+        </li>
+      </ul>
+    </nav>
+  </div>
 );
 
 export default Sidebar;
