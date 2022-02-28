49df20ce18ee8479dbe402b0aed75b08be3bd534
warugaki <warugaki.web.developer@gmail.com>
2022-03-01 01:06:54 +0900

    SidebarLink をmapを使って出力できるにする

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -3,28 +3,40 @@ import SidebarLink from './SidebarLink';
 
 import logo from '../assets/logo.svg';
 
-const Sidebar = () => (
-  <div className="sidebar">
-    <div className="sidebar__logo">
-      <img src={logo} alt="logo" />
-      <h1>React Admin Dashboard</h1>
+const Sidebar = () => {
+  const items = [
+    {
+      label: 'Dashboard',
+      url: '/',
+      boxIcons: 'bx bxs-dashboard',
+    },
+    {
+      label: 'Customers',
+      url: '/customers',
+      boxIcons: 'bx bxs-user-pin',
+    },
+  ];
+  return (
+    <div className="sidebar">
+      <div className="sidebar__logo">
+        <img src={logo} alt="logo" />
+        <h1>React Admin Dashboard</h1>
+      </div>
+      <nav>
+        <ul>
+          {items.map(({ label, url, boxIcons }, index) => (
+            // eslint-disable-next-line react/no-array-index-key
+            <li key={index}>
+              <SidebarLink to={url}>
+                <i className={boxIcons} />
+                {label}
+              </SidebarLink>
+            </li>
+          ))}
+        </ul>
+      </nav>
     </div>
-    <nav>
-      <ul>
-        <li>
-          <SidebarLink to="/">
-            <i className="bx bxs-dashboard" /> Dashboard
-          </SidebarLink>
-        </li>
-        <li>
-          <SidebarLink to="/customers">
-            <i className="bx bxs-user-pin" />
-            Customers
-          </SidebarLink>
-        </li>
-      </ul>
-    </nav>
-  </div>
-);
+  );
+};
 
 export default Sidebar;
