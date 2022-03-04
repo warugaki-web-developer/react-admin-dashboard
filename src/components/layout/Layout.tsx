9d4b11fad89efc3de42f8a66e9c922b42d5d1467
warugaki <warugaki.web.developer@gmail.com>
2022-03-04 22:01:00 +0900

    レイアウトのスタイリング

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,13 +1,42 @@
 import React from 'react';
+import { css } from '@emotion/react';
 
 import { Outlet } from 'react-router-dom';
-
 import Sidebar from '../Sidebar';
 
+const layoutStyle = css`
+  & {
+    display: flex;
+    width: 100%;
+    min-height: 100vh;
+    color: var(--txt-color);
+  }
+
+  .layout__side {
+    width: 300px;
+  }
+
+  .layout__content {
+    flex: 1;
+    background-color: var(--second-bg);
+  }
+
+  .layout__content-main {
+    flex: 1;
+    padding: 30px;
+  }
+`;
+
 const Layout = () => (
-  <div>
-    <Sidebar />
-    <Outlet />
+  <div className="layout" css={layoutStyle}>
+    <div className="layout__side">
+      <Sidebar />
+    </div>
+    <div className="layout__content">
+      <main className="layout__content-main">
+        <Outlet />
+      </main>
+    </div>
   </div>
 );
 
